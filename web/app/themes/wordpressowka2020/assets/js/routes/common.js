79de99ed171f6590a809b20d06ffb7dcf95d48c3
Maciej Palmowski <m.palmowski@osomstudio.com>
2020-05-13 00:58:51 +0200

    Hiding site name box
    - moved site-name to separate template

--- web/app/themes/wordpressowka2020/assets/js/routes/common.js
+++ web/app/themes/wordpressowka2020/assets/js/routes/common.js
@@ -1,40 +1,55 @@
 export default {
   init() {
-	class ThemeSwitcher {
-		constructor() {
-			// define some state variables
-			this.hasLocalStorage = typeof Storage !== 'undefined'
-			this.activeTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light';
 
-			document.documentElement.setAttribute('data-theme', this.activeTheme)
-
-			var btn = document.getElementById( 'themeswitcher' );
-			btn.addEventListener('click', () => this.setTheme());
-		}
-
-		setTheme() {
-			this.activeTheme = ( this.activeTheme === 'light' ) ? 'dark' : 'light';
-			// set the theme id on the <html> element...
-			document.documentElement.setAttribute('data-theme', this.activeTheme)
-
-			// and save the selection in localStorage for later
-			if (this.hasLocalStorage) {
-				localStorage.setItem("theme", this.activeTheme)
-			}
-		}
-	}
-
-	// this whole thing only makes sense if custom properties are supported -
-	// so let's check for that before initializing our switcher.
 	if (window.CSS && CSS.supports('color', 'var(--fake-var)')) {
 		new ThemeSwitcher()
 	}
 
-
-
+	siteHeader();
 
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
   },
 };
+
+class ThemeSwitcher {
+	constructor() {
+		// define some state variables
+		this.hasLocalStorage = typeof Storage !== 'undefined'
+		this.activeTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light';
+
+		document.documentElement.setAttribute('data-theme', this.activeTheme)
+
+		var btn = document.getElementById( 'themeswitcher' );
+		btn.addEventListener('click', () => this.setTheme());
+	}
+
+	setTheme() {
+		this.activeTheme = ( this.activeTheme === 'light' ) ? 'dark' : 'light';
+		// set the theme id on the <html> element...
+		document.documentElement.setAttribute('data-theme', this.activeTheme)
+
+		// and save the selection in localStorage for later
+		if (this.hasLocalStorage) {
+			localStorage.setItem("theme", this.activeTheme)
+		}
+	}
+}
+
+function siteHeader() {
+	var header = document.getElementById( 'site-name' );
+	var hasLocalStorage = typeof Storage !== 'undefined';
+	var isClosed = localStorage.getItem('headerStatus') ? localStorage.getItem('headerStatus') : false;
+	var closeButton = document.getElementById( 'site-name-close' );
+
+	console.log( isClosed );
+	closeButton.addEventListener('click', () => {
+		header.classList.toggle( 'hide' );
+		localStorage.setItem( 'headerStatus', true );
+	});
+
+	if ( ! isClosed ) {
+		header.classList.toggle( 'hide' );
+	}
+}
