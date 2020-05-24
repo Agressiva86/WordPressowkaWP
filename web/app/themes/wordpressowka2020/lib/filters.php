daf25e8816d798c111bfd939e6d8b618b8fc0ee6
Maciej Palmowski <m.palmowski@osomstudio.com>
2020-05-24 21:38:12 +0200

    Dodano brakujące rzeczy

--- web/app/themes/wordpressowka2020/lib/filters.php
+++ web/app/themes/wordpressowka2020/lib/filters.php
@@ -1,36 +1,56 @@
 <?php
-use Mexitek\PHPColors\Color;
-
-function darken( $color, $amount = '' ) {
-	$color = new Color($color);
-	if( $amount != '' ){
-		$ret   = '#' . $color->darken($amount);
-	} else {
-		$ret = $color;
-	}
-	return $ret;
-}
-
-function lighten( $color, $amount = '' ) {
-	$color = new Color($color);
-	if( $amount != '' ){
-		$ret   = '#' . $color->lighten($amount);
-	} else {
-		$ret = $color;
-	}
-	return $ret;
-}
-
-add_filter('timber/twig', 'add_to_twig');
+add_filter( 'timber/twig', 'add_to_twig' );
 
 function add_to_twig( $twig ) {
 	/* this is where you can add your own functions to twig */
-	$twig->addExtension(new Twig_Extension_StringLoader());
-	$twig->addFilter(new Twig_SimpleFilter('lighten', 'lighten'));
-	$twig->addFilter(new Twig_SimpleFilter('darken', 'darken'));
+	$twig->addExtension( new Twig\Extension\StringLoaderExtension() );
+	$twig->addFilter( new Twig\TwigFilter( 'fb_share_url', 'fb_share_url' ) );
+	$twig->addFilter( new Twig\TwigFilter( 'tt_share_url', 'tt_share_url' ) );
+	$twig->addFilter( new Twig\TwigFilter( 'ln_share_url', 'ln_share_url' ) );
 	return $twig;
 }
 
 // Display the links to the general feeds: Post and Comment Feed.
-remove_action('wp_head', 'feed_links_extra', 3);
-remove_action('wp_head', 'feed_links', 2);
+remove_action( 'wp_head', 'feed_links_extra', 3 );
+remove_action( 'wp_head', 'feed_links', 2 );
+
+/**
+ * Share url to FB
+ *
+ * @param string $url
+ * @return string
+ * @todo other params
+ */
+function fb_share_url( $url ) {
+    $url = 'https://www.facebook.com/sharer.php?u='. urlencode( $url );
+    return $url;
+}
+
+/**
+ * Share url to TT
+ *
+ * @param string $url
+ * @param string $title
+ * @return string
+ * @todo other params
+ */
+function tt_share_url( $url, $title='' ) {
+    $url = 'https://twitter.com/intent/tweet?url='. urlencode( $url );
+
+    if ( $title != '' ) {
+        $url .= '&text='.urlencode( $title );
+    }
+    return $url;
+}
+
+/**
+ * Share url to Linkdin
+ *
+ * @param string $url
+ * @return string
+ * @todo other params
+ */
+function ln_share_url( $url ) {
+    $url = 'https://www.linkedin.com/shareArticle?url='. urlencode( $url );
+    return $url;
+}
--- web/app/themes/wordpressowka2020/tpl-nsl.php
+++ web/app/themes/wordpressowka2020/tpl-nsl.php
@@ -7,4 +7,9 @@
 
 $context = Timber::get_context();
 $context['post'] = Timber::get_posts()[0];
+$args                   = array(
+	'posts_per_page' => 1,
+	'post_type'		 => 'post',
+);
+$context['other_posts'] = Timber::get_posts( $args, 'OwlPost' );
 Timber::render('views/templates/nsl.twig', $context);
