457ba13a5eeaa99d9c6484e6fec27e78643ba06e
Maciej Palmowski <m.palmowski@freshpixels.pl>
2021-06-09 21:47:59 +0200

    added compatibility with bedrock

--- config/application.php
+++ config/application.php
@@ -72,6 +72,8 @@ Config::define('SECURE_AUTH_SALT', env('SECURE_AUTH_SALT'));
 Config::define('LOGGED_IN_SALT', env('LOGGED_IN_SALT'));
 Config::define('NONCE_SALT', env('NONCE_SALT'));
 
+Config::define('buddy_webhook', env('buddy_webhook'));
+
 /**
  * Custom Settings
  */
